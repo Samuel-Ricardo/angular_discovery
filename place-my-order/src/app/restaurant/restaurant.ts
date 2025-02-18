1205b6b17f7e7225556d67a1fad0db6e27e1b94b
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-02-18 16:34:00 -0300

    [ :sparkles: ] | create: interface - restaurant (app::model)

---
+++ place-my-order/src/app/restaurant/restaurant.ts
@@ -0,0 +1,19 @@
+interface Item {
+  name: string;
+}
+
+interface Menu {
+  lunch: Item[];
+}
+
+interface Address {
+  street: string;
+}
+
+interface Images {
+  thumbnail: string;
+}
+
+export interface Restaurant {
+  name: string;
+}
