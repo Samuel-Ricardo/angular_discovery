9aeef3f56f3b61d1ed0152cae73cc50dbc59397c
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-02-18 16:01:18 -0300

    [ :white_check_mark: ] | test: router - navbar (app::test)

--- place-my-order/src/app/app.component.spec.ts
+++ place-my-order/src/app/app.component.spec.ts
@@ -1,6 +1,12 @@
 import { Location } from '@angular/common';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +15,8 @@ import { RestaurantComponent } from './restaurant/restaurant.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +26,8 @@ describe('AppComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
+    location = TestBed.inject(Location);
+    router = TestBed.inject(Router);
   });
 
   it('should create the app', () => {
@@ -34,13 +44,11 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Place My Order App: Coming Soon!',
+      'place-my-order.com',
     );
   });
 
   it('should render the HomeComponent with router navigates to "/" path', fakeAsync(() => {
-    const location: Location = TestBed.inject(Location);
-    const router: Router = TestBed.inject(Router);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     router.navigate(['']).then(() => {
@@ -50,13 +58,54 @@ describe('AppComponent', () => {
   }));
 
   it('should render the RestaurantsComponent with router navigates to "/restaurants" path', fakeAsync(() => {
-    const location: Location = TestBed.inject(Location);
-    const router: Router = TestBed.inject(Router);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     router.navigate(['restaurants']).then(() => {
       expect(location.path()).toBe('/restaurants');
       expect(compiled.querySelector('pmo-restaurant')).not.toBe(null);
     });
+  }));
+
+  it('should have the home navigation link href set to "/"', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const homeLink = compiled.querySelector('li a');
+    const href = homeLink?.getAttribute('href');
+    expect(href).toEqual('/');
+  });
+
+  it('should have the restaurants navigation link href set to "/restaurants"', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const restaurantsLink = compiled.querySelector('li:nth-child(2) a');
+    const href = restaurantsLink?.getAttribute('href');
+    expect(href).toEqual('/restaurants');
+  });
+
+  it('should make the home navigation link class active when the router navigates to "/" path', fakeAsync(() => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    router.navigate(['']);
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+
+    const homeLinkLi = compiled.querySelector('li');
+    expect(homeLinkLi?.classList).toContain('active');
+    expect(compiled.querySelectorAll('.active').length).toBe(1);
+    flush();
+  }));
+
+  it('should make the restaurants navigation link class active when the router navigates to "/restaurants" path', fakeAsync(() => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    router.navigate(['restaurants']);
+    fixture.detectChanges();
+    tick();
+    fixture.detectChanges();
+
+    expect(location.path()).toBe('/restaurants');
+    const restaurantsLinkLi = compiled.querySelector('li:nth-child(2)');
+    expect(restaurantsLinkLi?.classList).toContain('active');
+    expect(compiled.querySelectorAll('.active').length).toBe(1);
+    flush();
   }));
 });
