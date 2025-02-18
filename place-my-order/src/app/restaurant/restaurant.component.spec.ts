c7e4b324ba9b95b6efe2c9126f97fe686d40cf37
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-02-18 14:13:31 -0300

    [ :white_check_mark: ] | test: component - restaurant (app::test)

--- place-my-order/src/app/restaurant/restaurant.component.spec.ts
+++ place-my-order/src/app/restaurant/restaurant.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { RestaurantComponent } from './restaurant.component';
 
@@ -8,10 +13,9 @@ describe('RestaurantComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RestaurantComponent]
-    })
-    .compileComponents();
-    
+      declarations: [RestaurantComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RestaurantComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +24,29 @@ describe('RestaurantComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should not show any restaurants markup if no restaurants', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.restaurant')).toBe(null);
+  });
+
+  it('should have two .restaurant divs', fakeAsync((): void => {
+    fixture.detectChanges();
+    tick(501);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const restaurantDivs = compiled.getElementsByClassName('restaurant');
+    const hoursDivs = compiled.getElementsByClassName('hours-price');
+    expect(restaurantDivs.length).toEqual(2);
+    expect(hoursDivs.length).toEqual(2);
+  }));
+
+  it('should display restaurant information', fakeAsync((): void => {
+    fixture.detectChanges();
+    tick(501);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.restaurant h3')?.textContent).toContain(
+      'Poutine Palace',
+    );
+  }));
 });
