a94ff02c01200a4c9c84d3f961d5ed043a504698
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-02-19 14:39:40 -0300

    [ :art: ] | create: component - reataurant > forms (app::component)

--- place-my-order/src/app/restaurant/restaurant.component.ts
+++ place-my-order/src/app/restaurant/restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Restaurant } from './restaurant';
 import { ResponseData, RestaurantService } from './restaurant.service';
 
@@ -13,18 +14,49 @@ export interface Data {
   styleUrl: './restaurant.component.css',
 })
 export class RestaurantComponent implements OnInit {
+  form: FormGroup<{
+    state: FormControl<string>;
+    city: FormControl<string>;
+  }> = this.createForm();
+
   restaurants: Data = {
     value: [],
     isPending: false,
   };
 
-  constructor(private restaurantService: RestaurantService) {}
+  states = {
+    isPending: false,
+    value: [
+      { name: 'Illinois', short: 'IL' },
+      { name: 'Wisconsin', short: 'WI' },
+    ],
+  };
+
+  cities = {
+    isPending: false,
+    value: [{ name: 'Springfield' }, { name: 'Madison' }],
+  };
+
+  constructor(
+    private restaurantService: RestaurantService,
+    private fb: FormBuilder,
+  ) {}
 
   ngOnInit(): void {
     this.restaurants.isPending = true;
     this.restaurantService.getRestaurants().subscribe((res: ResponseData) => {
       this.restaurants.value = res.data;
       this.restaurants.isPending = false;
+    });
+  }
+
+  createForm(): FormGroup<{
+    state: FormControl<string>;
+    city: FormControl<string>;
+  }> {
+    return this.fb.nonNullable.group({
+      state: { value: '', disabled: false },
+      city: { value: '', disabled: false },
     });
   }
 }
