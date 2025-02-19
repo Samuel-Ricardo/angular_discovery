44bca938cc609dd5fb86270a0b1783d1446e895c
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-02-19 13:39:22 -0300

    [ :sparkles: ] | update: component - restaurant [logic] (app::ui)

--- place-my-order/src/app/restaurant/restaurant.component.ts
+++ place-my-order/src/app/restaurant/restaurant.component.ts
@@ -1,4 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant';
+import { ResponseData, RestaurantService } from './restaurant.service';
+
+export interface Data {
+  value: Restaurant[];
+  isPending: boolean;
+}
+
+@Component({
+  selector: 'pmo-restaurant',
+  templateUrl: './restaurant.component.html',
+  styleUrl: './restaurant.component.css',
+})
+export class RestaurantComponent implements OnInit {
+  restaurants: Data = {
+    value: [],
+    isPending: false,
+  };
+
+  constructor(private restaurantService: RestaurantService) {}
+
+  ngOnInit(): void {
+    this.restaurants.isPending = true;
+    this.restaurantService.getRestaurants().subscribe((res: ResponseData) => {
+      this.restaurants.value = res.data;
+      this.restaurants.isPending = false;
+    });
+  }
+}
 
 const fakeRestaurants = [
   {
@@ -94,18 +123,3 @@ const fakeRestaurants = [
     _id: 'Ar0qBJHxM3ecOhcr',
   },
 ];
-
-@Component({
-  selector: 'pmo-restaurant',
-  templateUrl: './restaurant.component.html',
-  styleUrl: './restaurant.component.css',
-})
-export class RestaurantComponent implements OnInit {
-  restaurants: any[] = [];
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.restaurants = fakeRestaurants;
-    }, 500);
-  }
-}
