703f8d75e06cde26f758bd13d4706e6ee76773fc
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-02-19 14:00:25 -0300

    [ :art: ] | create: component - reataurant > loading (app::component)

--- place-my-order/src/app/restaurant/restaurant.component.ts
+++ place-my-order/src/app/restaurant/restaurant.component.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant';
 import { ResponseData, RestaurantService } from './restaurant.service';
 
+export interface Data {
+  value: Restaurant[];
+  isPending: boolean;
+}
+
 @Component({
   selector: 'pmo-restaurant',
   templateUrl: './restaurant.component.html',
   styleUrl: './restaurant.component.css',
 })
 export class RestaurantComponent implements OnInit {
-  restaurants: Restaurant[] = [];
+  restaurants: Data = {
+    value: [],
+    isPending: false,
+  };
 
   constructor(private restaurantService: RestaurantService) {}
 
   ngOnInit(): void {
+    this.restaurants.isPending = true;
     this.restaurantService.getRestaurants().subscribe((res: ResponseData) => {
-      this.restaurants = res.data;
+      this.restaurants.value = res.data;
+      this.restaurants.isPending = false;
     });
   }
 }
